574ffdbec290148a26345656541ae1c2c467ccbe
arda15bice-creator <arda15bice@gmail.com>
2025-09-29 14:36:46 +0300

    Add files via upload

---
+++ rectangle.js
@@ -0,0 +1,16 @@
+package square1;
+
+public class squarecircum {
+	
+	public static void main(String[]args) {
+	
+	int kenar1 = 5;
+	int kenar2 = 10;
+	int cevre = kenar1 * 2 + kenar2 * 2;
+	int alan = kenar1 * kenar2;
+	
+	System.out.println("dikdörtgen cevresi:"+ cevre);
+	System.out.println("dikdörtgen alan:"+ alan);
+
+}
+}
